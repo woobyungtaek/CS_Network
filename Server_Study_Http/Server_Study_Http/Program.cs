cb94fb31916f2537e8931f71865e389608f0f99d
woobyungtaek <woobyungtaek@gmail.com>
2021-10-23 20:53:42 +0900

    http server

---
+++ Server_Study_Http/Server_Study_Http/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Server_Study_Http
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // using문은 2가지 경우, 지시자의 경우 네임스페이스를 사용표시
+            // 아래와 같은경우 해당 리소스가 범위를 벗어나면 자동으로 Dispose해준다.
+            using(Socket srvSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp))
+            {
+                Console.WriteLine("http://localhost:8000으로 방문해 보세요.");
+
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 8000);
+
+                srvSocket.Bind(endPoint);
+                srvSocket.Listen(10);
+
+                while(true)
+                {
+                    Socket clntSocket = srvSocket.Accept();
+                    ThreadPool.QueueUserWorkItem(HttpProcessFunc, clntSocket);
+                }
+
+            }            
+        }
+
+        private static void HttpProcessFunc(object obj)
+        {
+            Socket socket = obj as Socket;
+
+            byte[] reqBuf = new byte[4096];
+            socket.Receive(reqBuf);
+
+            string header = "http/1.0 200 OK\nContent-Type: text/html; charset=UTF-8\r\n\r\n";
+            string body = "<html><body><mark>테스트 HTML</mark>웹 페이지 입니다.</body></html>";
+
+            byte[] respBuf = Encoding.UTF8.GetBytes(header + body);
+
+            socket.Send(respBuf);
+
+            socket.Close();
+        }
+    }
+}
---
+++ Server_Study_Http/Server_Study_Http/Program_사용안함.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server_Study_Http
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddr = Dns.GetHostEntry("www.naver.com").AddressList[0];
+
+            EndPoint serverEP = new IPEndPoint(ipAddr, 80);
+            socket.Connect(serverEP);
+
+            string request = "GET / HTTP/1.0\r\nHost: www.naver.com\r\n\r\n";
+            byte[] sendBuffer = Encoding.UTF8.GetBytes(request);
+
+            socket.Send(sendBuffer);
+
+            MemoryStream ms = new MemoryStream();
+            while(true)
+            {
+                byte[] rcvBuffer = new byte[4096];
+
+                int nRecv = socket.Receive(rcvBuffer);
+                if (nRecv == 0)
+                {
+                    break;
+                }
+
+                ms.Write(rcvBuffer, 0, nRecv);
+            }
+
+            socket.Close();
+
+            string response = Encoding.UTF8.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+            Console.WriteLine(response);
+
+            File.WriteAllText("naverpage.html", response);
+        }
+    }
+}
